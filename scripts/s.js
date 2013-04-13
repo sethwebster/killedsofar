5c87ea8c7897b875dc7da45f3cf88f0b5ef789e7
Webster <sethwebster@gmail.com>
2013-04-13 02:04:19 -0700

    Complete js refactor

--- scripts/s.js
+++ scripts/s.js
@@ -1,90 +1,112 @@
-var killedCounter = {
+var KilledCounter = function() {
+	
+	this.selectedImage = null;
+	this.images = Array();
+	
+	this.settings = {
+		tickLength : 50,
+		domain : "killedsofar.com"
+	}
 
-}
-
-var domain = "killedsofar.com";
-if (document.domain!=domain && document.domain!="")
-	window.location = "http://"+domain;
-
-var ticklength = 50;
-var total = 59000000000;
-var perday = total / 365;
-var perhour = perday / 24;
-var permin = perhour / 60;
-var persec = permin / 60;
-var permilli = persec / 1000;
-var pertick = persec / (1000/ticklength);
-var startingVal = 0;
-var currDate = new Date();
-var doy = dayofyear(currDate);
-var isInitialized = false;
-
-var images = Array();
-images[0] = {path:"images/Slaughterhouse_cattle_bodies.jpg",credit:""};
-images[1] = {path:"images/Slaughterhouse-Floor.jpg",credit:""};
-images[2] = {path:"images/BeautifulCow.jpg",credit:"Jim Champion, Flickr"};
-images[3] = {path:"images/Lamb.jpg",credit:"Donald Macleod, Flickr"};
-images[4] = {path:"images/Piglet.jpg",credit:"Sander van der Wel, Flickr"};
-
-var selectedImage = images[Math.floor((Math.random()*images.length))];
-
-function dayofyear(d) {   // d is a Date object
-	var yn = d.getFullYear();
-	var mn = d.getMonth();
-	var dn = d.getDate();
-	var d1 = new Date(yn,0,1,12,0,0); // noon on Jan. 1
-	var d2 = new Date(yn,mn,dn,12,0,0); // noon on input date
-	var ddiff = Math.round((d2-d1)/864e5);
-	return ddiff+1; 
+	this.start = function() {
+		var domain = "killedsofar.com";
+		if (document.domain!=domain && document.domain!="")
+			window.location = "http://"+domain;
+	
+		this.initializeCounterValues();
+		this.initializeImages();
+		this.initializeInterface();
+		
+	}
+	
+	this.initializeImages = function() {
+		this.images.push({path:"images/Slaughterhouse_cattle_bodies.jpg",credit:"Anonymous"});
+		this.images.push({path:"images/Slaughterhouse-Floor.jpg",credit:"Anonymous"});
+		this.images.push({path:"images/BeautifulCow.jpg",credit:"Jim Champion, Flickr"});
+		this.images.push({path:"images/Lamb.jpg",credit:"Donald Macleod, Flickr"});
+		this.images.push({path:"images/Piglet.jpg",credit:"Sander van der Wel, Flickr"});
+	}
+	
+	this.initializeInterface = function() {
+		this.selectedImage = this.images[Math.floor((Math.random()*this.images.length))];
+		$("html").css("background-image","url("+this.selectedImage.path+")");
+		$("#version").load("DEPLOYED_VERSION.txt");
+		$("#credit").html("Image Credit: "+this.selectedImage.credit);
+		
+		setTimeout(function() {
+			$("#floater").fadeIn({duration:3000});
+		}, 3000);
+		setTimeout(function() {
+			$("#comments-wrapper").fadeIn({duration:1000});
+		}, 5000);
+		this.doUpdate();
+	}
+	
+	this.initializeCounterValues = function() {
+		this.countervalues = {
+		};
+		this.countervalues.ticklength = 50;
+		this.countervalues.total = 59000000000;
+		this.countervalues.perday = this.countervalues.total / 365;
+		this.countervalues.perhour = this.countervalues.perday / 24;
+		this.countervalues.permin = this.countervalues.perhour / 60;
+		this.countervalues.persec = this.countervalues.permin / 60;
+		this.countervalues.permilli = this.countervalues.persec / 1000;
+		this.countervalues.pertick = this.countervalues.persec / (1000/this.settings.tickLength);
+		this.currentVal = 0;
+		this.currDate = new Date();
+		this.countervalues.doy = this.dayOfYear(this.currDate);
+	}	
+	
+	this.positionCounter = function() {
+		var pointer = window;
+		if ($(window).height() != $(document).height())
+		{
+			// Mozilla HACK
+			pointer = document;
+		}
+		var top = ($(pointer).height()/2) - $("#floater").height()/2; 
+		$("#floater").css("top",top+"px");
+	}
+	
+	this.updateCurrentValue = function() {
+		
+		this.currDate = new Date();
+	
+		this.currentVal = (this.countervalues.perday*this.countervalues.doy)
+			+(this.currDate.getHours()*this.countervalues.perhour)
+			+(this.currDate.getMinutes()*this.countervalues.permin)
+			+(this.currDate.getSeconds()*this.countervalues.persec)
+			+(this.currDate.getMilliseconds()*this.countervalues.permilli);
+	}
+	
+	this.doUpdate = function() {
+		this.updateCurrentValue();
+		this.positionCounter();
+		$("#total").html(numberWithCommas(Math.round(this.currentVal)));
+		var _this = this;
+		var t = setTimeout(function() {
+			_this.doUpdate();
+		}, this.settings.tickLength);
+	}
+	
+	this.dayOfYear = function(d) {
+			var yn = d.getFullYear();
+			var mn = d.getMonth();
+			var dn = d.getDate();
+			var d1 = new Date(yn,0,1,12,0,0); // noon on Jan. 1
+			var d2 = new Date(yn,mn,dn,12,0,0); // noon on input date
+			var ddiff = Math.round((d2-d1)/864e5);
+			return ddiff+1; 
+	}
 }
 
 function numberWithCommas(x) {
 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
-
-function getCurrentValue() {
-	var currDate = new Date();
-	
-	startingVal = (perday*doy)
-		+(currDate.getHours()*perhour)
-		+(currDate.getMinutes()*permin)
-		+(currDate.getSeconds()*persec)
-		+(currDate.getMilliseconds()*permilli);
-
-	return startingVal;
-}	
-
-function intialize() {
-	if (isInitialized)
-		return;
-	var selectedImage = images[Math.floor((Math.random()*images.length))];
-	$("html").css("background-image","url("+selectedImage.path+")");
-	isInitialized = true;
-	$("#version").load("DEPLOYED_VERSION.txt");
-	$("#credit").html("Image Credit: "+selectedImage.credit);
-	var pointer = window;
-	if ($(window).height() != $(document).height())
-	{
-		// Mozilla HACK
-		pointer = document;
-	}
-	var top = ($(pointer).height()/2) - $("#floater").height()/2; 
-	$("#floater").css("top",top+"px");
-}
-
-function doUpdate() {
-	$("#total").html(numberWithCommas(Math.round(getCurrentValue())));
-	var t = setTimeout(doUpdate,ticklength);	
-}
-  
+ 
 $(document).ready(function() { 
-	intialize();
-	setTimeout(function() {
-		$("#floater").fadeIn({duration:3000});
-	}, 3000);
-	setTimeout(function() {
-		$("#comments-wrapper").fadeIn({duration:1000});
-	}, 5000);
-	doUpdate(); 
+	var k = new KilledCounter();
+	k.start();
 });
 
